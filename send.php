c9cf9e3fc2c1a3077c1fef5c4c042165e70f5943
Yasuhiro Okuda <y-okuda@firstserver.co.jp>
2017-08-10 12:29:46 +0900

    Update send.php

--- send.php
+++ send.php
@@ -9,7 +9,10 @@ $content = "送れましたか？";
  
 // 2.差出人を日本語表示
 $mailfrom="From:" .mb_encode_mimeheader("奥田 泰弘") ."<y-okuda@firstserver.co.jp>";
+
+// 3.Content-transfer-encodingヘッダを指定
+$email_header .= "Content-transfer-encoding: 8bit\n";
  
-// 3.上記（送信先、件名、本文、差出人）を日本語でメール送信実行
+// 4.上記（送信先、件名、本文、差出人）を日本語でメール送信実行
 mb_send_mail($mailto, $subject, $content, $mailfrom);
 echo '送信完了！';
